c2e18317a3911414ff87dffc0fc4fe26760f7bc2
Adrian Galisteo mula <agaliste@c2r7s3.42madrid.com>
2020-11-28 18:56:02 +0100

    Rush00: Corrected norme erros + added left 42 Headders in main.c and rush03.c

--- Rush00/rush03.c
+++ Rush00/rush03.c
@@ -1,40 +1,52 @@
-void    ft_putchar(char c);
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush03.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agaliste <agaliste@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/28 18:53:59 by agaliste          #+#    #+#             */
+/*   Updated: 2020/11/28 18:54:01 by agaliste         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void    rush(int x, int y)
+void	ft_putchar(char c);
+
+void	rush(int x, int y)
 {
-    int px;
-    int py;
+	int px;
+	int py;
 
-    px = 0;
-    py = 0;
-    while (py < y)
-    {
-        while (px < x)
-        {
-            if (px == 0 && (py < 1 || py == y - 1))
-            {
-                ft_putchar('A');
-            }
-            else if ((py == 0 || py == y - 1) && (px > 0 && px < x - 1))
-            {
-                ft_putchar('B');
-            }
-            else if ((px == 0 || px == x - 1) && (py > 0 && py < y - 1))
-            {
-                ft_putchar('B');
-            }
-            else if (px == x - 1 && (py < 1 || py == y - 1))
-            {
-                ft_putchar('C');
-            }
-            else
-            {
-                ft_putchar(' ');
-            }
-            px++;
-        }
-        py++;
-        px = 0;
-        ft_putchar('\n');
-    }   
+	px = 0;
+	py = 0;
+	while (py < y)
+	{
+		while (px < x)
+		{
+			if (px == 0 && (py < 1 || py == y - 1))
+			{
+				ft_putchar('A');
+			}
+			else if ((py == 0 || py == y - 1) && (px > 0 && px < x - 1))
+			{
+				ft_putchar('B');
+			}
+			else if ((px == 0 || px == x - 1) && (py > 0 && py < y - 1))
+			{
+				ft_putchar('B');
+			}
+			else if (px == x - 1 && (py < 1 || py == y - 1))
+			{
+				ft_putchar('C');
+			}
+			else
+			{
+				ft_putchar(' ');
+			}
+			px++;
+		}
+		py++;
+		px = 0;
+		ft_putchar('\n');
+	}
 }
