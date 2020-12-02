faa642d1e485167baacf9834f91e627205cf186f
Adrian Galisteo mula <agaliste@c2r7s3.42madrid.com>
2020-12-02 18:03:41 +0100

    C02: Added various changes / additions

--- C02/ex10/ft_strlcpy.c
+++ C02/ex10/ft_strlcpy.c
@@ -6,27 +6,37 @@
 /*   By: agaliste <agaliste@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/02 13:25:32 by agaliste          #+#    #+#             */
-/*   Updated: 2020/12/02 16:36:12 by agaliste         ###   ########.fr       */
+/*   Updated: 2020/12/02 18:02:33 by agaliste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+static int		ft_strlen(char *str)
+{
+	int		i;
+
+	i = 0;
+	while (str[i])
+		i += 1;
+	return (i);
+}
+
+static char		*ft_strncpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
 	i = 0;
-	while (src[i] != '\0' && i < size)
+	while ((i < n) && src[i])
 	{
 		dest[i] = src[i];
-		++i;
+		i += 1;
 	}
-	if (i < size && src[i] == '\0')
-	{
-		while (dest[i] != '\0')
-		{
-			dest[i] = '\0';
-			++i;
-		}
-	}
+	while (i < n)
+		dest[i++] = '\0';
 	return (dest);
+}
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	ft_strncpy(dest, src, size);
+	return (ft_strlen(src));
 }
--- C02/ex11/ft_putstr_non_printable.c
+++ C02/ex11/ft_putstr_non_printable.c
@@ -6,27 +6,45 @@
 /*   By: agaliste <agaliste@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/02 16:38:39 by agaliste          #+#    #+#             */
-/*   Updated: 2020/12/02 16:45:02 by agaliste         ###   ########.fr       */
+/*   Updated: 2020/12/02 18:02:34 by agaliste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_str_is_printable(char *str)
+int			ft_putchar(char c)
 {
-	int i;
+	write(1, &c, 1);
+}
 
-	i = 0;
-	while (*str != '\0')
+static void	ft_print_hexa(char np)
+{
+	char	*hex;
+
+	hex = "0123456789abcdef";
+	if (np > 16)
 	{
-		if (*str <= ' ' && *str >= '~')
-		{
-			write(1, &str, 1);
-		}
-		*str++;
-	}
-	if (i > 0)
-	{
-		return (0);
+		ft_print_hexa(np / 10);
+		ft_print_hexa(np % 10);
 	}
 	else
-		return (1);
+		ft_putchar(hex[np]);
+}
+
+void		ft_putstr_non_printable(char *str)
+{
+	int		i;
+
+	i = 0;
+	while (str[i])
+	{
+		if ((str[i] < 32 && str[i] > 0) || str[i] == 127)
+		{
+			ft_putchar('\\');
+			if (str[i] < 16)
+				ft_putchar('0');
+			ft_print_hexa(str[i]);
+		}
+		else
+			ft_putchar(str[i]);
+		i += 1;
+	}
 }
