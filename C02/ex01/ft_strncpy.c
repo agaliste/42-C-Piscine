f4824e994d8ba3625daed458f9aff28653b7db6b
Adrian Galisteo mula <agaliste@c2r7s3.42madrid.com>
2020-12-02 17:40:46 +0100

    C02: Made some changes

--- C02/ex01/ft_strncpy.c
+++ C02/ex01/ft_strncpy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: agaliste <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: agaliste <agaliste@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/30 12:52:51 by agaliste          #+#    #+#             */
-/*   Updated: 2020/11/30 13:50:32 by agaliste         ###   ########.fr       */
+/*   Updated: 2020/12/02 17:27:48 by agaliste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,11 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 	unsigned int i;
 
 	i = 0;
-	while (src[i] != '\0' && i < n)
+	while (src != '\0' && (i < n))
 	{
 		dest[i] = src[i];
-		++i;
+		i++;
 	}
-	if (i < n && src[i] == '\0')
-	{
-		while (dest[i] != '\0')
-		{
-			dest[i] = '\0';
-			++i;
-		}
-	}
+	dest[i] = '\0';
 	return (dest);
 }
--- C02/ex10/ft_strlcpy.c
+++ C02/ex10/ft_strlcpy.c
@@ -6,7 +6,27 @@
 /*   By: agaliste <agaliste@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/02 13:25:32 by agaliste          #+#    #+#             */
-/*   Updated: 2020/12/02 15:56:20 by agaliste         ###   ########.fr       */
+/*   Updated: 2020/12/02 16:36:12 by agaliste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int i;
+
+	i = 0;
+	while (src[i] != '\0' && i < size)
+	{
+		dest[i] = src[i];
+		++i;
+	}
+	if (i < size && src[i] == '\0')
+	{
+		while (dest[i] != '\0')
+		{
+			dest[i] = '\0';
+			++i;
+		}
+	}
+	return (dest);
+}
