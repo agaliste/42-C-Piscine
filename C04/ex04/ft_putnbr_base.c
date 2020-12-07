709af9c8fbbee6a38d597b35b5e22c57ed2c1208
Adrian Galisteo mula <agaliste@c2r7s3.42madrid.com>
2020-12-07 15:10:10 +0100

    C04: Added exercises

--- C04/ex04/ft_putnbr_base.c
+++ C04/ex04/ft_putnbr_base.c
@@ -1,42 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: agaliste <agaliste@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/12/07 07:58:22 by agaliste          #+#    #+#             */
-/*   Updated: 2020/12/07 14:56:58 by agaliste         ###   ########.fr       */
+/*   Created: 2020/12/07 15:08:00 by agaliste          #+#    #+#             */
+/*   Updated: 2020/12/07 15:08:07 by agaliste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-
-int	ft_atoi(char *str)
+void ft_putnbr_base(int nbr, char *base)
 {
-	int res;
-	int neg;
-	int n;
 
-	res = 0;
-	neg = 0;
-	n = 0;
-	while (*str != '\0')
-	{
-		if (*str < 48 || *str > 57)
-		{
-			if (n > 0 && (*(str - 1) >= 48 && *(str - 1) <= 57))
-				break ;
-			if (*str == '-')
-				neg++;
-		}
-		n++;
-		if (*str >= 48 && *str <= 57)
-			res = res * 10 + *str - '0';
-		str++;
-	}
-	if (neg % 2 == 0)
-		return (res);
-	else
-		return (res * -1);
 }
