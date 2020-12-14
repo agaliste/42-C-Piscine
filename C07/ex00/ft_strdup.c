7ce8bb4feeaee4e8da0b118c32dc89878415ad91
Adrian Galisteo mula <agaliste@c2r8s3.42madrid.com>
2020-12-14 16:59:54 +0100

    Added C07

---
+++ C07/ex00/ft_strdup.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agaliste <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/14 15:32:49 by agaliste          #+#    #+#             */
+/*   Updated: 2020/12/14 16:33:28 by agaliste         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strdup(char *src)
+{
+	int		i;
+	int		len;
+	char	*dest;
+
+	len = 0;
+	i = 0;
+	while (src[len] != '\0')
+		len++;
+	if (!(dest = (char *)malloc(sizeof(char) * (len + 1))))
+		return (0);
+	while (i < len)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
