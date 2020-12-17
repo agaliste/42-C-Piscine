6117450909f6ed4086a19e0e4bd705b3aa0e854c
Adrian Galisteo mula <agaliste@c2r8s3.42madrid.com>
2020-12-17 17:25:15 +0100

    C07: Fixed ex01 and ex02 not compiling

--- C07/ex01/ft_range.c
+++ C07/ex01/ft_range.c
@@ -6,7 +6,7 @@
 /*   By: agaliste <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/14 15:34:42 by agaliste          #+#    #+#             */
-/*   Updated: 2020/12/15 09:32:51 by agaliste         ###   ########.fr       */
+/*   Updated: 2020/12/17 17:11:29 by agaliste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	*ft_range(int min, int max)
 {
 	int i;
 	int *tab;
-	int lenght;
+	int length;
 
 	length = max - min + 1;
 	i = 0;
--- C07/ex02/ft_ultimate_range.c
+++ C07/ex02/ft_ultimate_range.c
@@ -6,7 +6,7 @@
 /*   By: agaliste <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/15 09:20:05 by agaliste          #+#    #+#             */
-/*   Updated: 2020/12/15 09:32:36 by agaliste         ###   ########.fr       */
+/*   Updated: 2020/12/17 17:21:00 by agaliste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,14 @@ int	*ft_ultimate_range(int **range, int min, int max)
 {
 	int i;
 	int *tab;
-	int lenght;
 
-	length = max - min + 1;
 	i = 0;
-	tab = malloc(sizeof(*tab) * lenght);
+	tab = (int*)malloc(sizeof(*tab) * (max - min));
 	if (min >= max)
 	{
-		ret = NULL;
+		*range = NULL;
 		return (0);
 	}
-	if (!(*tab))
-		return (-1);
 	while (min < max)
 	{
 		tab[i] = min;
